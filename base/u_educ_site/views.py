71cb97edd1576c1b641f634567e079072ea88be5
Philly Olowo <olowophilly11@gmail.com>
2024-05-30 12:18:10 +0300

    Updated views file for login logiut and home

--- base/u_educ_site/views.py
+++ base/u_educ_site/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,5 +16,6 @@ def logout(request):
     logout(request)
     return redirect('login')
 
+@login_required(login_url='/login')
 def home(request):
     return render(request, "dashboard.html")
