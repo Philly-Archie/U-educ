822a78f25190c43e86bc5dec9eb1ae10b3215fd2
Philly Olowo <olowophilly11@gmail.com>
2024-06-10 13:20:17 +0300

    Registered sponsor preference model in admin site

--- base/u_educ_site/admin.py
+++ base/u_educ_site/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Role
+from .models import Role, SponsorPreferences
 
 # Register your models here.
 
 admin.site.register(Role)
+admin.site.register(SponsorPreferences)
