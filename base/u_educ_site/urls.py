178e64b7625dabb9dad9d96b8ea13393e90040fb
Philly Olowo <olowophilly11@gmail.com>
2024-06-13 14:02:21 +0300

    Added media upload static urls imports

--- base/u_educ_site/urls.py
+++ base/u_educ_site/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
@@ -27,3 +30,7 @@ urlpatterns = [
     # path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(
     #     template_name="auth/password_complete.html"), name="password_rest_complete"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
