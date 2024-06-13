e2040dfcb54116b8505bf66d3b61aba043de1cbf
Philly Olowo <olowophilly11@gmail.com>
2024-06-13 14:01:50 +0300

    Removed weight calculations from models.py and add them to views functions

--- base/u_educ_site/models.py
+++ base/u_educ_site/models.py
@@ -16,24 +16,18 @@ class Role(models.Model):
         return self.role
     
 
-def calculate_tuition_weight(min_tuition, max_tuition):
-    if min_tuition is not None and max_tuition is not None:
-        range_tuition = max_tuition - min_tuition
-        weight = range_tuition / (max_tuition + min_tuition)
-        return weight
-    return 0.0
-
 class SponsorPreferences(models.Model):
     firstName = models.CharField(max_length=200)
     lastName = models.CharField(max_length=200)
     email = models.EmailField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    phoneNumber = models.CharField(max_length=15, blank=True, null=True)
     address = models.CharField(max_length=300, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
     state = models.CharField(max_length=100, blank=True, null=True)
     country = models.CharField(max_length=100, blank=True, null=True)
-    zip_code = models.CharField(max_length=20, blank=True, null=True)
+    zipCode = models.CharField(max_length=20, blank=True, null=True)
 
     preferred_student_gender = models.CharField(max_length=10, choices=[
         ('male', 'Male'),
@@ -68,30 +62,10 @@ class SponsorPreferences(models.Model):
     weight_student_courses = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
     weight_tuition = models.FloatField(default=0.0)
 
+    # profileImage = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.email})'
-    
-    def calculate_weights(self):
-        weights = {}
-
-        if self.preferred_student_gender == 'female':
-            weights['preferred_student_gender'] = 1.0
-        else:
-            weights['preferred_student_gender'] = 0.5
-
-        if self.student_courses == 'sciences':
-            weights['student_courses'] = 1.0
-        else:
-            weights['student_courses'] = 0.8
-        
-        weights['tuition_amount'] = calculate_tuition_weight(self.tuition_amount_min, self.tuition_amount_max)
-
-        self.save()
-
-        
-    def average_weight(self):
-        weights = [self.weight_gender, self.weight_courses]
-        return sum(weights) / len(weights)
