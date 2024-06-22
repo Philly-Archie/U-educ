5cf52b209aedc82d2f75de98b918169360d71e17
Philly Olowo <olowophilly11@gmail.com>
2024-06-22 20:37:58 +0800

    Added weight calculation in models file

--- base/u_educ_site/models.py
+++ base/u_educ_site/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator
+from django.utils.timezone import now
+import os
+from math import ceil
 
 
 # Create your models here.
@@ -15,7 +18,6 @@ class Role(models.Model):
     def __str__(self):
         return self.role
     
-
 class SponsorPreferences(models.Model):
     firstName = models.CharField(max_length=200)
     lastName = models.CharField(max_length=200)
@@ -35,6 +37,12 @@ class SponsorPreferences(models.Model):
         ('any', 'Any'),
     ], default='any', help_text="Preferred gender of the student.")
 
+    preferred_student_courses = models.CharField(max_length=20, choices=[
+        ('sciences', 'Sciences'),
+        ('arts', 'Arts'),
+        ('any', 'Any'),
+    ], default='any', help_text="Preferred course of study for the student.")
+
     tuition_amount_min = models.DecimalField(
         max_digits=10, 
         decimal_places=2, 
@@ -46,26 +54,55 @@ class SponsorPreferences(models.Model):
     tuition_amount_max = models.DecimalField(
         max_digits=10, 
         decimal_places=2, 
-        validators=[MaxValueValidator(0)], 
+        validators=[MinValueValidator(0)], 
         blank=True, 
         null=True,
         help_text="Maximum tuition amount the sponsor is willing to provide."
     )
 
-    preferred_student_courses = models.CharField(max_length=20, choices=[
-        ('sciences', 'Sciences'),
-        ('arts', 'Arts'),
-        ('any', 'Any'),
-    ], default='any', help_text="Preferred course of study for the student.")
 
     weight_student_gender = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
     weight_student_courses = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
     weight_tuition = models.FloatField(default=0.0)
 
-    # profileImage = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    profileImage = models.ImageField(upload_to = 'profile_images/', blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.email})'
+    
+    def calculate_tuition_weight(self):
+        if self.tuition_amount_min is not None and self.tuition_amount_max is not None:
+            range_tuition = self.tuition_amount_max - self.tuition_amount_min
+            weight = range_tuition / (self.tuition_amount_max + self.tuition_amount_min)
+            return weight
+        return 0.0   
+
+    def calculate_weights(self):
+        if self.preferred_student_gender == 'female':
+            self.weight_student_gender = 1.0
+        else:
+            self.weight_student_gender = 0.5
+
+        if self.preferred_student_courses == 'sciences':
+            self.weight_student_courses = 1.0
+        else:
+            self.weight_student_courses = 0.8
+        
+        self.weight_tuition = self.calculate_tuition_weight()
+
+        self.save()
+
+    def average_weight(self):
+        weights = [self.weight_student_gender, self.weight_student_courses, self.weight_tuition]
+        my_average_weight = sum(weights) / len(weights)
+        rounded_average_weight = round(my_average_weight, 3)
+        return (abs(rounded_average_weight))
+    
+class Mappings(models.Model):
+    sponsor = models.ForeignKey(User, on_delete=models.CASCADE)
+    studentFullName = models.CharField(max_length=500)
+    studentEmail = models.EmailField()
+    studentPhoneNumber = models.CharField(max_length=15, blank=True, null=True)
