f3800378624c07ed1bbb576781706aa1afcb5194
Philly Olowo <olowophilly11@gmail.com>
2024-06-25 04:18:24 +0800

    Registered mappings model in the admin py file for django

--- base/u_educ_site/admin.py
+++ base/u_educ_site/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Role, SponsorPreferences
+from .models import Role, SponsorPreferences, Mappings
 
 # Register your models here.
 
 admin.site.register(Role)
 admin.site.register(SponsorPreferences)
+admin.site.register(Mappings)
