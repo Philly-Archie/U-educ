28d98653987499b944827aee1fd8d1a901a8adcd
Philly Olowo <olowophilly11@gmail.com>
2024-06-30 10:24:20 +0300

    Registered models in the admin.py file

--- base/apis/admin.py
+++ base/apis/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import UserModel, DetailsModel, FamEducationModel
+
+# Register your models here.
+
+admin.site.register(UserModel)
+admin.site.register(DetailsModel)
+admin.site.register(FamEducationModel)
