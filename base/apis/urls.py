9886932095ba863a7c05a314af2df9653b66faad
Philly Olowo <olowophilly11@gmail.com>
2024-06-30 10:26:27 +0300

    Added urls for api calls in the urls.py

--- base/apis/urls.py
+++ base/apis/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 
 from .views import SponsorPreferenceList, SponsorPreferenceDetail
+from .import views
 
 urlpatterns = [
     path("", SponsorPreferenceList.as_view()),
     path("<int:pk>/", SponsorPreferenceDetail.as_view()),
+    path('create_user/', views.create_user, name='create_user'),
+    path('create_details/', views.create_details, name='create_details'),
+    path('create_fam_education/', views.create_fam_education, name='create_fam_education'),
+
 ]
