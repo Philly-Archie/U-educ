aa371a72a98107e5223ede212a7a6cded50ef573
Philly Olowo <olowophilly11@gmail.com>
2024-06-30 10:25:57 +0300

    Added student serializer for apis in serializers.py

--- base/apis/serializers.py
+++ base/apis/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from u_educ_site import models
 
+from .models import UserModel, FamEducationModel, DetailsModel
+
 
 class SponsorPreferenceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,5 +11,22 @@ class SponsorPreferenceSerializer(serializers.ModelSerializer):
             'firstName',
             'lastName',
             'profileImage',
+            'address',
+            'phoneNumber',
         )
         model = models.SponsorPreferences
+
+class UserModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = '__all__'
+
+class DetailsModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetailsModel
+        fields = '__all__'
+
+class FamEducationModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FamEducationModel
+        fields = '__all__'
